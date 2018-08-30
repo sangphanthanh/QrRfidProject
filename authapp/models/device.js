502ab69b6ca90f8dce8524c1940cd7e835036a8b
Kelvin <thanhsang94@gmail.com>
2018-08-30 12:54:18 +0700

    log

--- authapp/models/device.js
+++ authapp/models/device.js
@@ -39,6 +39,7 @@ module.exports.putClockStatusByMac = function(Mac, ClockStatus, callback){
 }
 //PUT DoorStatus By MacAddress
 module.exports.putDoorStatusByMac = function(Mac, DoorStatus, callback){
+    console.log('doorStatus: ' + DoorStatus);
     const query = {Mac:Mac}
     Device.findOneAndUpdate(query,{$set:{DoorStatus:DoorStatus}},callback);
 }
