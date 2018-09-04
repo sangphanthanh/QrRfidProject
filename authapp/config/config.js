b05a146987beb567a2d0bd0b8e580252b7548bea
Kelvin <thanhsang94@gmail.com>
2018-09-04 17:23:08 +0700

    update fn

--- authapp/config/config.js
+++ authapp/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
 	database: 'mongodb://localhost:27017/authapp',
 	secret: 'AuthenticationTokenKey',
 	port: '8000',
-	TokenTime: '604800',
+	TokenTime: '7d',
 	roundSalt: 10 //set salt 10 round for bcrypt
 }
