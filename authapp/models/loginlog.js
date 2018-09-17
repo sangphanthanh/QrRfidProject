ee2a0e72b60744e9a4e174e61e69401045cbc73a
Kelvin <thanhsang94@gmail.com>
2018-09-17 11:31:59 +0700

    add and verify log

--- authapp/models/loginlog.js
+++ authapp/models/loginlog.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-//LogSchema
+/**
+ * Create log Schema
+ */
 const LoginLogSchema = mongoose.Schema({
     Device: {type: String, require: true},
     TypeOfServices: {type: String, require: true},
@@ -12,23 +14,40 @@ const LoginLogSchema = mongoose.Schema({
 });
 const LoginLog = module.exports = mongoose.model('loginlog',LoginLogSchema);
 
+/**
+ * add Log
+ * @param {*} newLog 
+ * @param {*} callback 
+ */
 module.exports.addLoginLog = function(newLog, callback){
     var day = new Date();
     newLog.Info.Timestamp = day;
     newLog.save(callback);
 }
-//trace all log
+
+/**
+ * Show Log
+ * @param {*} callback 
+ */
 module.exports.traceLog = function(callback){
     LoginLog.find(callback);
 }
 
-//find by device
+/**
+ * List log by Mac Dec Device
+ * @param {*} macDevice 
+ * @param {*} callback 
+ */
 module.exports.findByMacDevice = function(macDevice,callback){
     const query = {Device: macDevice}
     LoginLog.findOne(query,callback);
 }
 
-//find by service
+/**
+ * List log by Services
+ * @param {*} service 
+ * @param {*} callback 
+ */
 module.exports.findByServices = function(service,callback){
     const query = {TypeOfServices: service}
     LoginLog.findOne(query,callback);
