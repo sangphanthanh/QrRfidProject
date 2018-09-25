5924aaf983478f031bad19c4d83d533b60920b1f
Kelvin <thanhsang94@gmail.com>
2018-09-25 10:45:04 +0700

    add systemlog

--- authapp/models/loginlog.js
+++ authapp/models/loginlog.js
@@ -60,7 +60,7 @@ module.exports.findByServices = function(service,callback){
  */
 module.exports.system_log = function(msg,callback){
     var datetime = new Date();
-    var file = 'authapp.log';
+    var file = 'authapp_system_log.txt';
     var text = '['+ datetime+'] '+msg+'\r\n';
     fs.appendFile(file,text,function(err){
         if(err) throw err;
