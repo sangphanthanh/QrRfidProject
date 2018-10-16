daa74b44f37b5984ed5b98054d9a01d1ea0811e5
Kelvin <thanhsang94@gmail.com>
2018-10-16 17:55:42 +0700

    comment mailler

--- authapp/routes/users.js
+++ authapp/routes/users.js
@@ -136,35 +136,33 @@ router.post('/authenticate', (req, res, next) => {
 					msg: config.ST_Code18
 				});
 
-				let transporter = nodemailler.createTransport(smtpTransport({
-					service: 'Gmail',
-					auth: {
-						user: config.MAILLER_CONF_USERNAME, // generated ethereal user
-						pass: config.MAILLER_CONF_PASSWORD // generated ethereal password
-					}
-				}));
+				// let transporter = nodemailler.createTransport(smtpTransport({
+				// 	service: 'Gmail',
+				// 	auth: {
+				// 		user: config.MAILLER_CONF_USERNAME, // generated ethereal user
+				// 		pass: config.MAILLER_CONF_PASSWORD // generated ethereal password
+				// 	}
+				// }));
 
-				// setup email data with unicode symbols
-				let mailOptions = {
-					from: '"[QR Adquest Asia]" <adquestasia.kelvin@gmail.com>', // sender address
-					to: 'adquestasia.kelvin@gmail.com', // list of receivers
-					subject: '[QR Adquest Asia] [ Send Mail test]', // Subject line
-					text: 'Content for Email', // plain text body
-					html: '<b>Email for testing QR Team?</b>' // html body
-				};
+				// // setup email data with unicode symbols
+				// let mailOptions = {
+				// 	from: '"[QR Adquest Asia]" <adquestasia.kelvin@gmail.com>', // sender address
+				// 	to: 'adquestasia.kelvin@gmail.com', // list of receivers
+				// 	subject: '[QR Adquest Asia] [ Send Mail test]', // Subject line
+				// 	text: 'Content for Email', // plain text body
+				// 	html: '<b>Email for testing QR Team?</b>' // html body
+				// };
 
-				// send mail with defined transport object
-				transporter.sendMail(mailOptions, (error, info) => {
-					if (error) {
-						return console.log(error);
-					}
-					console.log('Message sent: %s', info.messageId);
-					// Preview only available when sending through an Ethereal account
-					console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+				// // send mail with defined transport object
+				// transporter.sendMail(mailOptions, (error, info) => {
+				// 	if (error) {
+				// 		return console.log(error);
+				// 	}
+				// 	console.log('Message sent: %s', info.messageId);
+				// 	// Preview only available when sending through an Ethereal account
+				// 	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-				});
-
-
+				// });
 				break;
 		}
 	})
