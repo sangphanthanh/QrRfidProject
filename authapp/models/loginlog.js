9edc66beac8898cc7120868e164458d3ca7bb78d
Kelvin <thanhsang94@gmail.com>
2018-10-17 11:53:43 +0700

    fix bug send mail

--- authapp/models/loginlog.js
+++ authapp/models/loginlog.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
+const nodemailler = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const config = require('../config/config');
 /**
  * Create log Schema
  */
@@ -66,3 +69,35 @@ module.exports.findByServices = function(service,callback){
 //         if(err) throw err;
 //     });
 // }
+
+
+module.exports.sendMailLoginFail = function(email,callback){
+    let transporter = nodemailler.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: config.MAILLER_CONF_USERNAME, // generated ethereal user
+            pass: config.MAILLER_CONF_PASSWORD // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"[QR Adquest Asia]"', // sender address
+        to: 'adquestasia.kelvin@gmail.com', // list of receivers
+        subject: '[QR Adquest Asia] [ Warning Login Notification]', // Subject line
+        text: 'Content for Email', // plain text body
+        html: '<b>Someone try to access account: </b>'+ email + '<br />'
+        + '<b> At: </b>' +   Date()// html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    });
+}
--- authapp/routes/users.js
+++ authapp/routes/users.js
@@ -135,35 +135,9 @@ router.post('/authenticate', (req, res, next) => {
 					success: false,
 					msg: config.ST_Code18
 				});
-
-				let transporter = nodemailler.createTransport({
-					service: 'Gmail',
-					auth: {
-						user: config.MAILLER_CONF_USERNAME, // generated ethereal user
-						pass: config.MAILLER_CONF_PASSWORD // generated ethereal password
-					}
-				});
-
-				// setup email data with unicode symbols
-				let mailOptions = {
-					from: '"[QR Adquest Asia]"', // sender address
-					to: 'adquestasia.kelvin@gmail.com', // list of receivers
-					subject: '[QR Adquest Asia] [ Warning Login Notification]', // Subject line
-					text: 'Content for Email', // plain text body
-					html: '<b>Someone try to access account: </b>'+ username + '<br />'
-					+ '<b> At: </b>' +   Date()// html body
-				};
-
-				// send mail with defined transport object
-				transporter.sendMail(mailOptions, (error, info) => {
-					if (error) {
-						return console.log(error);
-					}
-					console.log('Message sent: %s', info.messageId);
-					// Preview only available when sending through an Ethereal account
-					console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-				});
+				//send Mail 
+				loginlog.sendMailLoginFail(username);
+				
 				break;
 		}
 	})
